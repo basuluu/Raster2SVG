bfe870edae9f1b08a589f6c89d3c47ae4c9ee50c
severok1077@mail.ru <severok1077@mail.ru>
2020-08-11 20:36:24 +0300

    refactor code, split big function on sub-functions, add code comments, delete useless code

--- src/trace.py
+++ src/trace.py
@@ -1,12 +1,10 @@
-from scipy.ndimage import label, find_objects
+from scipy.ndimage import label
 import cv2
+import numpy as np
 
 from copy import deepcopy
 
-from figure import *
 
-
-# TODO: create function to get contour fill color
 # TODO: think about corners of not rectangle figure
 # TODO: still have bug of white lines beside figures
 
@@ -14,34 +12,27 @@ def trace(img):
     img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
     outlines = []
     for color in np.unique(img_gray):
-        color_mask = get_image_color_mask(img_gray, color)
-        # search group of neighbour pixels, [0,1,1,0,1,1,0] -> [0,1,1,0,2,2,0]
-        matrix, count = label(color_mask)
-        bw_mask = get_bw_mask(matrix)
-
+        bw_mask = get_image_bw_mask(img_gray, color)
         outlines += get_outline(img, bw_mask)
     return outlines
 
-# Create mask for our img/color -> [[0, 0, color, color, 0, 0]]
-def get_image_color_mask(img_gray, color):
+# Create black and white mask, where black(255) is group of pixels with searched color
+def get_image_bw_mask(img_gray, color):
     color_mask = deepcopy(img_gray)
     # If color == 0, we need to invert (0 -> 1) and (other colors -> 0)
     if color == 0:
         color_mask = color_mask * 0
-        color_mask[img_gray==color] = 1
+        color_mask[img_gray==color] = 255
     else:
         color_mask[color_mask!=color] = 0
-    return color_mask
-
-# create black and white mask, where black(255) is group of pixels with searched color
-def get_bw_mask(matrix):
-    matrix[matrix!=0] = 255
-    return matrix.astype('uint8')
+        color_mask[color_mask==color] = 255
+    return color_mask.astype('uint8')
 
 def get_outline(img, bw_mask):
     fill = None
     outlines = []
     # search countrs and approx it
+    # need two approx groups for collect corner points with closes neigbors
     countrs_approx, _ = cv2.findContours(bw_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_TC89_KCOS)
     countrs_full, _   = cv2.findContours(bw_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
@@ -56,28 +47,44 @@ def get_outline(img, bw_mask):
         if len(countr_approx_points) < 3:
             continue
 
-        outline_on_matrix = []
-        for point in countr_approx_points:
-            index = countr_full_points.index(point)
+        corners_with_neighbor_points = collect_neighbor_corner_points(
+            countr_approx_points, countr_full_points
+        )
 
-            if index == 0:
-                outline_on_matrix.append(point)
-                outline_on_matrix.append(countr_full_points[1])
-            else:
-                outline_on_matrix += countr_full_points[index-1:index+2]
-
-            # Close our contour for last dot
-            if point == countr_approx_points[-1]:
-                outline_on_matrix.append(countr_full_points[-1])
-                outline_on_matrix.append(countr_approx_points[0])
-
-            countr_full_points = countr_full_points[index+1:]            
-
-        red, green, blue = img[outline_on_matrix[0][1], outline_on_matrix[0][0]] 
-        fill = f"rgb({red}, {green}, {blue})"
+        if fill == None:
+            index = (corners_with_neighbor_points[0][1], corners_with_neighbor_points[0][0])
+            fill = get_fill_color(img, index)
 
         outlines.append({
-            'outline_points': outline_on_matrix,
+            'outline_points': corners_with_neighbor_points,
             'fill': fill,
         })
     return outlines
+
+# get corner points with closes neighbor
+# return list of coords [(0,0), (0, 1)...(1, 0)]
+# [[1, 1, 1, 1, 1],          [[1, 1, 0, 1, 1],
+#  [1, 1, 1, 1, 1],    <->    [1, 0, 0, 0, 1],
+#  [1, 1, 1, 1, 1],    <->    [1, 0, 0, 0, 1],
+#  [1, 1, 1, 1, 1]]           [1, 1, 0, 1, 1]]
+def collect_neighbor_corner_points(countr_approx_points, countr_full_points):
+    neighbor_corner_points = []
+    for point in countr_approx_points:
+        index = countr_full_points.index(point)
+        if index == 0:
+            neighbor_corner_points.append(point)
+            neighbor_corner_points.append(countr_full_points[1])
+        else:
+            neighbor_corner_points += countr_full_points[index-1:index+2]
+        # Close our contour for last dot
+        if point == countr_approx_points[-1]:
+            neighbor_corner_points.append(countr_full_points[-1])
+            neighbor_corner_points.append(countr_approx_points[0])
+
+        countr_full_points = countr_full_points[index+1:]  
+    return neighbor_corner_points
+
+# return value of svg fill attribute
+def get_fill_color(img, index):
+    red, green, blue = img[index] 
+    return f"rgb({red}, {green}, {blue})"
